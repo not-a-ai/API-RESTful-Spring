6cbdfaee70d0e553d7a3c75cfa4a99a963469bf1
Aléxia <alexia.cassia09@gmail.com>
2024-07-02 17:54:09 -0400

    first commit

---
+++ src/test/java/me/dio/API/RESTcom/Spring/ApiResTcomSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.API.RESTcom.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResTcomSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
